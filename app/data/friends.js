8a038acf7ee68c3ae58f7884a6b42fd9d0557f98
Johnny Dellaluna <johnnydellaluna@gmail.com>
2017-10-25 16:16:58 -0400

    Setting up HTML and routes
    Created objects in friends.js, set up get routes and post routes. Need to get value of user survey input saved and then find out how to compare it with other objects

---
+++ app/data/friends.js
@@ -0,0 +1,58 @@
+var friends = [
+	{
+        "name": "Kermit",
+        "photo": "https://images.thewest.com.au/publication/B88534421Z/GKT14JT4M.2-1.jpg?imwidth=800",
+        "scores": []
+
+
+    },
+
+    {
+        "name": "Miss Piggy",
+        "photo": "https://pbs.twimg.com/profile_images/662349173467975680/UxlUTQL0.jpg",
+        "scores": []
+
+
+    },
+
+    {
+        "name": "Gonzo",
+        "photo": "https://muppetmindset.files.wordpress.com/2013/09/de8b6-ggeneral155a.jpg",
+        "scores": []
+
+
+    },
+
+    {
+        "name": "Fozzie",
+        "photo": "https://vignette.wikia.nocookie.net/disney/images/1/13/Fozzie_Bear_Icon.jpg/revision/latest?cb=20131204222052",
+        "scores": []
+
+
+    },
+
+    {
+        "name": "Animal",
+        "photo": "https://i.pinimg.com/originals/47/92/0d/47920da09420a898382074c285c26f9d.jpg",
+        "scores": []
+
+
+    },
+
+    {
+        "name": "Lew Zealand",
+        "photo": "https://vignette4.wikia.nocookie.net/muppet/images/5/51/Lew_Zealand_blue.png/revision/latest?cb=20150520133751",
+        "scores": []
+
+
+    },
+
+    {
+        "name": "Beaker",
+        "photo": "https://i.pinimg.com/736x/21/cd/ae/21cdae369104bde3ab772b8a4ac4bd28--the-muppets-style-icons.jpg",
+        "scores": []
+
+
+    }
+
+];
